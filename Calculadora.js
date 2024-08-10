ec73776c6f6a32d7baeb99dd0b3688f1addcfc75
Javier Vera Flores <aczayacatl27@hotmail.com>
2024-08-10 02:11:00 -0600

    Primer pre-entrega-JS

---
+++ Calculadora.js
@@ -0,0 +1,9 @@
+alert("Bievenido a la calculadora de inters compuesto");
+
+class cliente {
+    Constructor (nombre, edad, inversionIncial){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.inversionIncial = inversionIncial;
+    }
+}
